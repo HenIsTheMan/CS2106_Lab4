e4fd20654fa25d65646dee5a13f0849b9494500b
HenIsTheMan <2169test2169@gmail.com>
2025-04-10 01:29:08 +0800

    3: No warnings

--- bitmap/mymalloc.h
+++ bitmap/mymalloc.h
@@ -1,3 +1,5 @@
+#include <stdio.h> //Added for size_t
+
 #define MEMSIZE 8         // Size of memory in bytes (was 64)
 
 // Used by the test harness
@@ -6,5 +8,5 @@ long get_index(void *ptr);
 // Debugging routine
 void print_memlist();
 
-void *mymalloc(size_t);
+void *mymalloc(size_t size);
 void myfree(void *);
